5057e073dd7b25b96821f26308ac2458c38ad2e6
jovo <joshuav@jhu.edu>
2009-08-05 00:11:55 -0400

    about to replace my results with yuriys

--- code/data_example.m
+++ code/data_example.m
@@ -1,53 +1,53 @@
-clear, clc
-load('~/Research/oopsi/meta-oopsi/data/tom/20081126_13_05_43_orientation_Bruno_reg1_ori1_135umdepth.mat')
-%%
-nrows=2;
-ncols=3;
-fs=12;
-figure(1), clf
+% clear, clc
+% load('~/Research/oopsi/meta-oopsi/data/tom/20081126_13_05_43_orientation_Bruno_reg1_ori1_135umdepth.mat')
+% %%
+% nrows=2;
+% ncols=3;
+% fs=12;
+% figure(1), clf
+% 
+% %%
+% a=Im.MeanFrame;
+% MeanFrame = (a-min(a(:)))/(max(a(:))-min(a(:)));
+% roi_edges = Im.roi_edges;
+% roi_edges(roi_edges>1)=1;
+% 
+% seg_frame = MeanFrame+roi_edges;
+% subplot(nrows,ncols,[1 4])
+% subimage(seg_frame)
+% set(gca,'XTick',[0 30 60 90 120])
+% colormap(gray)
+% title('Mean Frame','FontSize',fs)
+% 
+% tvec=1000:2800;
+% xtick=[0:900:tvec(end)];
+% fs=12;
+% xticklabel=xtick/30;
+% subplot(nrows,ncols,2)
+% plot(z1(detrend(double(Im.F(3,tvec))))+1,'k')
+% axis([0 max(tvec)-min(tvec) 0 2])
+% title('High SNR','FontSize',fs)
+% set(gca,'XTick',xtick,'XTickLabel',[])
+% set(gca,'YTick',[])
+% ylab=ylabel([{'$\mathbf{F}$'}; {''}; {''}; {'$\widehat{\mathbf{n}}$'}],'FontSize',fs,'Interpreter','latex');
+% set(ylab,'Rotation',0,'HorizontalAlignment','right','verticalalignment','middle')
+% 
+% subplot(nrows,ncols,5)
+% plot(z1(detrend(double(Im.F(4,tvec))))+1,'k')
+% axis([0 max(tvec)-min(tvec) 0 2])
+% title('Low SNR','FontSize',fs)
+% set(gca,'XTick',xtick,'XTickLabel',xticklabel,'FontSize',fs)
+% set(gca,'YTick',[])
+% ylab=ylabel([{'$\mathbf{F}$'}; {''}; {''}; {'$\widehat{\mathbf{n}}$'}],'FontSize',fs,'Interpreter','latex');
+% set(ylab,'Rotation',0,'HorizontalAlignment','right','verticalalignment','middle')
+
 
 %%
-a=Im.MeanFrame;
-MeanFrame = (a-min(a(:)))/(max(a(:))-min(a(:)));
-roi_edges = Im.roi_edges;
-roi_edges(roi_edges>1)=1;
-
-seg_frame = MeanFrame+roi_edges;
-subplot(nrows,ncols,[1 4])
-subimage(seg_frame)
-set(gca,'XTick',[0 30 60 90 120])
-colormap(gray)
-title('Mean Frame','FontSize',fs)
-
-tvec=1000:2800;
-xtick=[0:900:tvec(end)];
-fs=12;
-xticklabel=xtick/30;
-subplot(nrows,ncols,2)
-plot(z1(detrend(double(Im.F(3,tvec))))+1,'k')
-axis([0 max(tvec)-min(tvec) 0 2])
-title('High SNR','FontSize',fs)
-set(gca,'XTick',xtick,'XTickLabel',[])
-set(gca,'YTick',[])
-ylab=ylabel([{'$\mathbf{F}$'}; {''}; {''}; {'$\widehat{\mathbf{n}}$'}],'FontSize',fs,'Interpreter','latex');
-set(ylab,'Rotation',0,'HorizontalAlignment','right','verticalalignment','middle')
-
-subplot(nrows,ncols,5)
-plot(z1(detrend(double(Im.F(4,tvec))))+1,'k')
-axis([0 max(tvec)-min(tvec) 0 2])
-title('Low SNR','FontSize',fs)
-set(gca,'XTick',xtick,'XTickLabel',xticklabel,'FontSize',fs)
-set(gca,'YTick',[])
-ylab=ylabel([{'$\mathbf{F}$'}; {''}; {''}; {'$\widehat{\mathbf{n}}$'}],'FontSize',fs,'Interpreter','latex');
-set(ylab,'Rotation',0,'HorizontalAlignment','right','verticalalignment','middle')
-
-
-%%
-netSim.N=20;                    %number of cells
+netSim.N=100;                    %number of cells
 netSim.SP=0.10;                  %sparseness of connections
 netSim.scaleE=0.025;            %excitatiry abs scale
 netSim.scaleI=0.1;              %inhibitory abs scale
-netSim.balance=0.2;             %fraction of inhibitory neurons
+netSim.balance=0.1;             %fraction of inhibitory neurons
 netSim.T=6e5;                   %simulation duration, steps
 netSim.dt=0.001;                %simulation time-step
 netSim.FR=0.015;                %sampling time step
@@ -119,26 +119,27 @@ P.omega=60*(P.omega-min(P.omega(:)))/(max(P.omega(:))-min(P.omega(:)));
 
 
 %%
+figure(1), clf
 cmap=zeros(64,3);
 zero_point=33; 
 cmap(:,1)=[zeros(zero_point,1)' linspace(0,1,64-zero_point)];
 cmap(:,3)=[linspace(1,0,64-zero_point) zeros(1,zero_point)];
 
-subplot(nrows,ncols,[3 6])
+% subplot(nrows,ncols,[3 6])
 subimage(P.omega,cmap), 
 set(gca,'FontSize',fs)
 colormap(cmap),
 colorbar(...
-    'location','southoutside',...
+    'location','eastoutside',...
     'XTick',linspace(1,60,3),...
     'XTickLabel',[-1 0 1])
-set(gca,'XTick',[])
+% set(gca,'XTick',[])
 title('Weight Matrix')
 
 
 %%
 wh = [7 4];
 set(gcf,'PaperSize',wh,'PaperPosition',[0 0 wh],'Color','w');
-FigName = ['~/Research/oopsi/pop-oopsi/figs/data_example'];
+FigName = ['~/Research/oopsi/pop-oopsi/figs/W_matrix'];
 print('-depsc',FigName)
 print('-dpdf',FigName)
