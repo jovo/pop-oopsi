f54e1950d1e4e134970acea184884b1072ae7e68
jovo <joshuav@jhu.edu>
2009-08-10 11:13:10 -0700

    modified liams intro and yuriys results

--- code/fr_vs_J.m
+++ code/fr_vs_J.m
@@ -3,11 +3,15 @@ dt=1/60;
 FR=(1-exp(-exp(J)*dt))/dt;
 
 figure(1), clf
+
+hold on
+plot(J,5*ones(size(J)),'linewidth',2,'color',[.7 .7 .7])
+
 plot(J,FR,'k','linewidth',2)
 fs=18;
-ylab=ylabel([{'Expected Firing Rate'}],'FontSize',fs); % %,'Interpreter','latex');
+ylab=ylabel([{'$f(J)$: Firing Rate (Hz)'}],'FontSize',fs,'Interpreter','latex','fontname','arial');
 % set(ylab,'Rotation',0,'HorizontalAlignment','right','verticalalignment','middle')
-xlabel('$J$','Interpreter','latex','fontsize',fs)
+xlabel('$J$','Interpreter','latex','fontsize',fs,'fontname','arial')
 axis([-2 8 0 60])
 set(gca,'XTick',-2:2:8,'YTick',0:10:60)
 grid on
